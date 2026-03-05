88aca517056ee945bab1b7ce57582ca2b740d732
tomasbustf <figueroabustostomas@gmail.com>
2026-03-04 23:59:22 -0300

    Actualizacion en cards de team

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -16,8 +16,10 @@ export class TeamComponent {
       role: 'Founder & CEO',
       bio: 'Estudiante de cuarto año de Ingeniería Civil Informatica de la Universidad Católica del Maule.',
       gradient: 'linear-gradient(135deg,rgba(128,58,196,0.5),rgba(44,20,67,0.9))',
-      imageUrl: 'assets/images/tbf.jpg'
+      imageUrl: 'assets/images/tbf.jpg',
+      instagram: 'https://instagram.com/tomasbustf',
+      github: 'https://github.com/tomasbustf',
+      linkedin: 'https://www.linkedin.com/in/tom%C3%A1s-ignacio-bustos-figueroa-02a7b8186/'
     },
-
   ];
 }
